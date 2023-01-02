80a85e360dcb53ba9690aa3cd39caae839c52c9d
Ben Hagen <ben@ottomatic.io>
2023-01-02 15:25:12 +0100

    Fire longPress before key was released

--- lib/src/mixins.dart
+++ lib/src/mixins.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:meta/meta.dart';
 import 'package:streamdeck/streamdeck.dart';
 
@@ -7,12 +9,17 @@ import 'package:streamdeck/streamdeck.dart';
 /// Fires after [keyUp].
 mixin LongPressDetection on StreamDeckPluginAction {
   int? _downTime;
+  Timer? _longPressTimer;
 
   @override
   @mustCallSuper
   void keyDown(KeyDownEvent event) {
     super.keyDown(event);
     _downTime = DateTime.now().millisecondsSinceEpoch;
+    _longPressTimer = Timer(const Duration(seconds: 1), () {
+      _longPressTimer = null;
+      longPress();
+    });
   }
 
   @override
@@ -21,17 +28,17 @@ mixin LongPressDetection on StreamDeckPluginAction {
     super.keyUp(event);
     final downTime = _downTime;
 
-    if (downTime != null &&
-        DateTime.now().millisecondsSinceEpoch - downTime > 1000) {
-      longPress();
-    } else {
-      shortPress();
+    if (downTime != null) {
+      if (_longPressTimer != null) {
+        _longPressTimer?.cancel();
+        shortPress();
+      }
     }
   }
 
   /// Called after a key is released and was held for less than a second.
   void shortPress();
 
-  /// Called after a key is released and was held for more than a second.
+  /// Called after a key was held for more than a second.
   void longPress();
 }
